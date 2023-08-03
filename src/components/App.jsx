9c4bdb5c86d7a53d47ba878975d19f4eb5a0e225
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-08-03 14:00:40 -0400

    create filter, props and set state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,26 @@
 import GamesList from './GamesList'
 import SearchBarHeader from './SearchBarHeader'
 import gamesArray from '../data/games'
+import { useState } from 'react';
 
 function App() {
+
+  const [filterBy, setFilterBy] = useState("all")
+  const [filterByGenre, setFilterByGenre] = useState("")
+  
+  const filteredGames = gamesArray.filter(game => 
+  {
+    if (filterBy === "all") return true
+    if (filterBy === "played" ) return game.played
+    if (filterBy === "unplayed" ) return !game.played
+  } )
+
   return (
     <div className="App">
 
-      <SearchBarHeader />
+      <SearchBarHeader setFilterBy={setFilterBy} setFilterByGenre={setFilterByGenre } />
 
-      <GamesList gamesArray={gamesArray} />
+      <GamesList gamesArray={filteredGames} filterBy={filterBy} filterByGenre={filterByGenre} />
 
     </div>
   );
